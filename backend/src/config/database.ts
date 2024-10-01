d2aea20a335daff559b9264cc49ac0daf60e804a
Jorge Holguin <jorge.holguin1105@gmail.com>
2024-10-01 00:44:59 -0500

    Fix database.ts

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,8 +4,8 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   // Usa la variable de entorno o la URI local por defecto
   const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/peru-scooter-parts';
-  
-  // Define las opciones de conexión de mongoose
+
+  // Define las opciones de conexión de mongoose usando la variable de entorno USE_SSL
   const connectionOptions: mongoose.ConnectOptions = {
     ssl: process.env.USE_SSL === 'true', // Habilita SSL solo si USE_SSL es 'true'
     tlsAllowInvalidCertificates: process.env.USE_SSL === 'true' ? true : undefined, // Permitir certificados no válidos si se usa SSL
