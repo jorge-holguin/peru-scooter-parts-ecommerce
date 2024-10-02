c901d41d633fa15758dee722c85fbb71b1f0c8f8
Jorge Holguin <jorge.holguin1105@gmail.com>
2024-10-02 01:04:58 -0500

    add REACT_APP_API_URL in Frontend

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,5 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000'); // Asegúrate de usar la URL correcta
+// Usar variables de entorno para seleccionar la URL correcta
+const socket = io(process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000', {
+  transports: ['websocket', 'polling'],
+});
 
 export default socket;
